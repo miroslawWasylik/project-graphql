8b6a89bf1104def417b13459f8b8f0c3a0f0ef77
ladenisko <ladenmail@gmail.com>
2024-07-05 00:49:57 +0200

    add resolvers to project

--- server.js
+++ server.js
@@ -8,21 +8,17 @@ const { makeExecutableSchema } = require('@graphql-tools/schema');
 const typesArray = loadFilesSync('**/*', {
 	extensions: ['graphql']
 });
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
 
 const schema = makeExecutableSchema({
-	typeDefs: typesArray
+	typeDefs: typesArray,
+	resolvers: resolversArray
 });
-
-const root = {
-	products: require('./products/products.model'),
-	orders: require('./orders/orders.model'),
-};
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
 	schema: schema,
-	rootValue: root,
 	graphiql: true
 }));
 
